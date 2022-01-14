6673928682735372e7f3b8f9273e62bef3ecab36
idan157001 <idanalbam1@gmail.com>
2022-01-15 00:12:45 +0200

    ''

--- fivem.py
+++ fivem.py
@@ -181,7 +181,7 @@ async def on_ready():
                 info_channels= get_status_info(guild_id)
                 title_name,icon,IP = get_information(guild_id)
                 server = Server_info(IP)
-                req_json,max_players = server.send_request()[0],server.send_request()[1]
+                req_json,max_players = await server.send_request()
 
            
                
